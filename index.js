766c0ec53eca4187cf738e762c56ee3f62ac5f1a
kalingwu@gmail.com <kalingwu@gmail.com>
2018-02-13 10:34:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+function getFirstSelector(selector) {
+ return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+  for (var i = 0; i < lis.length; i++) {
+   lis[i].innerHTML =  parseInt(lis[i].innerHTML) + n
+  }
+}
+
+function deepestChild() {
+  const nodeList = document.querySelector('#grand-node').querySelectorAll('div')
+  var deepestChildNode = []
+  var nodeLength = nodeList.length
+    deepestChildNode = nodeList[nodeLength-1]
+  return deepestChildNode
+}
+
+/*function deepestChild() {
+  const nodeList = document.querySelector('#grand-node').querySelectorAll('div')
+  var deepestChildNode
+  for (var i = 0; i<nodeList.length-1 ; i++) {
+    deepestChildNode = nodeList[i]
+  }
+  return deepestChildNode.querySelector('div')
+}*/
+
+
